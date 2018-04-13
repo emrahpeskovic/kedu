f7e00a9694f618c063eb1df643689639c26686ec
Glazzy <32482767+Glazzy@users.noreply.github.com>
2018-04-13 23:31:06 +0200

    JAVASCRIPT YEES! DET VIRKER

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,5 @@
-var createbutton = document.getElementById('create');
-createbutton.onclick = saveInfo;
+//signup//
+
 
 var userData = [];
 
@@ -9,8 +9,28 @@ var efterNavn = document.getElementById('efternavn').value;
 var emailInput = document.getElementById('email').value;
 var passwordInput = document.getElementById('password').value;
 
-var infoObj = {fornavn:forNavn,efternavn:efterNavn,email:emailInput,password:passwordInput};
+var infoObj = {fornavn:forNavn , efternavn:efterNavn , email:emailInput, 'password':passwordInput};
 userData.push(infoObj);
-
 localStorage.setItem("bruger1",JSON.stringify(userData));
+}
+
+//login check//
+
+function checkInfo() {
+  var localData = localStorage.getItem('bruger1');
+  var convertedData = JSON.parse(localData);
+
+  var storedEmail = convertedData[0].email;
+  var storedPassword = convertedData[0].password;
+
+  var enteredEmail = document.getElementById('email');
+  var enteredPassword = document.getElementById('password')
+
+  if(enteredEmail.value !== storedEmail || enteredPassword.value !== storedPassword) {
+      alert('Fejl forkert email eller password');
+      return false;
+  }else {
+      alert('Sucess!');
+      location.href = "index.html";
+  }
 }
